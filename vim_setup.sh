af185fcfcca4d7e8c78df160292cfa37db50d312
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-05-25 21:58:07 +0200

    adding vim setup script

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,10 +1,11 @@
-#bin/bash
+#!bin/bash
 # Simple setup for vim
 
 # Download repository
-git clone https://github.com/ajwalkiewicz/MyConfigFiles.git
+git clone https://github.com/ajwalkiewicz/MyConfigFiles.git ~/Git/
 
 # Install Vundle - plugin manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-
+cp ~/Git/MyConfigFiles/vim/indent ~/.vim/
+cp ~/Git/MyConfigFiles/vim/colors ~/.vim/
