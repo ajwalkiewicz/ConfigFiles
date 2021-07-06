bfb0b5df7571ea8ce3bbca8e7195e8a03812dd08
Adam Walkiewicz <aj.walkiewicz@gmail.com>
2021-07-06 20:00:27 +0200

    deleting ald unnecessary files

--- post_install/05_final_tweks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Final tweaks
-
-git clone https://github.com/MichaelThessel/tilix-gruvbox.git
-cd tilix-gruvbox
-doas -- cp gruvbox-* /usr/share/tilix/schemes
-cd ..
-rm -rf tilix-gruvbox
